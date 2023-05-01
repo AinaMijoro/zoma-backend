eb7cbf2c77cf88c7066637cf7de19c97b4a21dd6
Aina Mijoro <aina.mijoro@gmail.com>
2023-05-01 17:19:51 +0200

    test

---
+++ models/users.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
+  token: String,
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
